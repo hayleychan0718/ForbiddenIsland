36b86096247a6b2e7f28135a0ab4dc1f96136afe
Hayley Chan <hayley.chan@ucdconnect.ie>
2020-10-13 23:09:12 +0100

    Added TreasureNames enum

---
+++ src/island/enums/TreasureNames.java
@@ -0,0 +1,19 @@
+package island.enums;
+
+public enum TreasureNames {
+	TheEarthStone ("The Earth Stone"),
+	TheStatueOfTheWind ("The Statue of The Wind"),
+	TheCrystalOfFire ("The Crystal of Fire"),
+	TheOceansChalice ("The Oceans Chalice");
+	
+	private final String name;
+	
+	private TreasureNames(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
