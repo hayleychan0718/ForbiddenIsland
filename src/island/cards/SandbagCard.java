979b82e686b30b56a406cdb9daa487c177bdac05
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-11-21 21:16:58 +0000

    Modified SandbagCard play() method: only print tiles that are flooded

--- src/island/cards/SandbagCard.java
+++ src/island/cards/SandbagCard.java
@@ -18,7 +18,7 @@ public class SandbagCard extends TreasureDeckCard{
 		int index = 0;
 
 		for(int i=0;i<listOfTiles.size();i++) {
-			if(listOfTiles.get(i).isFlooded() == true && listOfTiles.get(i).isPresent() == true) {
+			if(listOfTiles.get(i).isFlooded() == true) {
 				System.out.println(listOfTiles.get(i).getNameString() + "[" + index + "]");
 				index++;
 			}
