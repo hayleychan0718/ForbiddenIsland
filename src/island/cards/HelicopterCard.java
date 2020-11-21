9928eb7532f8994c950df1103e47740ffb41a1c3
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-11-21 22:43:23 +0000

    Added play() to HelicopterCard class

--- src/island/cards/HelicopterCard.java
+++ src/island/cards/HelicopterCard.java
@@ -1,5 +1,10 @@
 package island.cards;
 
+import java.util.ArrayList;
+
+import island.board.Board;
+import island.board.Tile;
+
 public class HelicopterCard extends TreasureDeckCard{
 	
 	public HelicopterCard() {
@@ -7,6 +12,29 @@ public class HelicopterCard extends TreasureDeckCard{
 	}
 	
 	public void play() {
+		System.out.println("Tiles you can move to:");
+		Board board = Board.getInstance();
+		ArrayList<Tile> list = board.listOfTiles();
+		int index = 0;
 		
+		for(int i=0; i<list.size();i++) {
+			System.out.println(list.get(i).getNameString() + "[" + index + "]");
+			index++;
+		}
+
 	}
+	
+	// Observer?
+	// IF: all players have all four treasures AND: everyone is on the Fool's Landing
+	// then: 
+
+	public void win() {
+		
+		/*
+		 * check if all players have the treasure
+		 * gameover() = true
+		 */
+	}
+	
+
 }
