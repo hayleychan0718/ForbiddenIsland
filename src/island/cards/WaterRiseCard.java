af3798c9eb22f1da7c3969ad7401af8d1400b0ca
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-11-21 10:56:37 +0000

    Added play() to WaterRiseCard

---
+++ src/island/cards/WaterRiseCard.java
@@ -0,0 +1,16 @@
+package island.cards;
+
+import gameLogic.WaterMeter;
+
+public class WaterRiseCard extends TreasureDeckCard{
+
+	
+	public void play() {
+		WaterMeter waterMeter = WaterMeter.getinstance();
+		FloodDeck floodDeck = FloodDeck.getInstance();
+
+		waterMeter.increaseWaterLevel();
+		floodDeck.reshuffle();
+	}
+	
+}
