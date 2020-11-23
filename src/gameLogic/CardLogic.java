dff37d23bf9aa9fa8243c45a950af015039052c6
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-11-23 21:45:14 +0000

    CardLogic: doHelicopter() complete. Able to move players to any tile

--- src/gameLogic/CardLogic.java
+++ src/gameLogic/CardLogic.java
@@ -1,7 +1,11 @@
 package gameLogic;
 
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import island.board.Board;
+import island.board.Tile;
 import island.cards.Hand;
 import island.cards.HelicopterCard;
 import island.cards.SandbagCard;
@@ -10,26 +14,28 @@ import island.cards.TreasureDeckCard;
 import island.cards.WaterRiseCard;
 import island.enums.TreasureNames;
 import players.Player;
+import players.PlayerList;
 
 public class CardLogic {
 
 	private Player player;
 	private Hand playerHand;
+	ArrayList<TreasureDeckCard> playableCards;
+
 	
 	public CardLogic(Player player) {
 		this.player=player;
-		playerHand=player.getHand();
-		
+		this.playerHand=player.getHand();
+		this.playableCards = playerHand.getCards();
 	}
 	
 	public void pickCard() {
 	//	int userInput();
-		ArrayList<TreasureDeckCard> playableCards = playerHand.getCards();
 		int picking = 0;
 
 		do{
 			System.out.println("Pick one of the cards you wish to play");
-		//	System.out.println("/n enter" + playableCards.size() + "[Return] to cancel action"); // Hayley: Not sure what this is doing
+		//	System.out.println("/n enter " + playableCards.size() + "[Return] to cancel action"); 
 			playerHand.printHand();
 			int userInput = PlayerAction.acceptableInput(0, playableCards.size());
 			
@@ -40,9 +46,14 @@ public class CardLogic {
 				playableCards.get(userInput).play();
 				playerHand.removeCard(playableCards.get(userInput));		
 				break;
-			case "Helicopter":
-				playableCards.get(userInput).play();
-				doHelicopter();
+			case "Helicopter Lift":
+				System.out.println("Do you want to:\n[0] Move player(s) to another tile? or\n[1] Lift off Fool's Landing?");
+				int in = PlayerAction.acceptableInput(0, 1);
+				if(in == 0) {
+					doHelicopter(userInput);
+				}
+				else
+					// TODO: Implement Lift Off
 				playerHand.removeCard(playableCards.get(userInput));		
 				break;
 			case "Sandbag":
@@ -70,9 +81,45 @@ public class CardLogic {
 		}while(picking != 0);
 	}
 	
-	public void doHelicopter() {
+	public void doHelicopter(int userInput) {
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		ArrayList<Tile> listOfTiles = Board.getInstance().listOfTiles();
+		PlayerList playerList = PlayerList.getInstance();
+		ArrayList<Player> chosenPlayers = new ArrayList<Player>();
+		String yn = "y";
+		boolean repeat = true;
+		
+		playableCards.get(userInput).play();
+		
+		System.out.println("Which tile do you want to move to? ");
+		int tileIndex = PlayerAction.acceptableInput(0, listOfTiles.size());
+
+		while(repeat == true){
+			System.out.println("Who do you want to move to " + listOfTiles.get(tileIndex).getNameString() + "?");
+			playerList.printListOfPlayers();
+			int playerIndex = PlayerAction.acceptableInput(0, playerList.getListOfPlayers().size());
+			if(chosenPlayers.contains(playerList.getPlayer(playerIndex))) {
+				System.out.println(playerList.getPlayer(playerIndex).getName() + " is already chosen.");
+			}
+			else
+				chosenPlayers.add(playerList.getPlayer(playerIndex));			
+			System.out.println("Do you want to choose another player? (Y/N)");
+		    yn = s.nextLine();
+		    if(yn.equals("y") || yn.equals("Y")) 
+		    	repeat = true;
+		    else if(yn.equals("n") || yn.equals("N"))
+		    	repeat = false;
+		}
+		
+		for(int i=0; i<chosenPlayers.size();i++) {
+			System.out.print("Moving " + chosenPlayers.get(i).getName() + " to " + listOfTiles.get(tileIndex).getNameString() +  "...\n");
+			chosenPlayers.get(i).movePlayerPawn(listOfTiles.get(tileIndex));
+		}		
+		
 		
 	}
+	
 	
 	public void doSandbag() {
 		
@@ -84,9 +131,13 @@ public class CardLogic {
 	 */
 	public static void main (String args[]) {
 		Player player = new Player("Hayley", 1);
+		Player player2 = new Player("Ben", 2);
+		PlayerList playerList = PlayerList.getInstance();
+		playerList.addPlayer(player);
+		playerList.addPlayer(player2);
 		SandbagCard card = new SandbagCard();
 		Hand theHand = player.getHand();
-		//theHand.printHand();
+		
 		theHand.addCard(new HelicopterCard());
 		theHand.addCard(card);
 		theHand.addCard(new WaterRiseCard());
