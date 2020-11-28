dc4937c59a06942d980753939a88326c9d78f398
liamfitzz <liam.fitzgerald@ucdconnect.ie>
2020-11-28 15:32:24 +0000

    Added two methods to select player and card, transferred acceptable input

--- src/gameLogic/PlayerAction.java
+++ src/gameLogic/PlayerAction.java
@@ -5,92 +5,94 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-import island.board.Board;
+import island.cards.Card;
 import island.cards.Hand;
-import players.Engineer;
+import island.cards.TreasureDeckCard;
 import players.*;
 import island.board.*;
-
+import utility.*;
 public class PlayerAction {
 
 	private Player player;
 	private Board board;
+	private Hand hand;
+	private int userInput;
+	private Tile selectedTile;
 
 	public PlayerAction(Player player) {
 		this.player=player;
 		this.board= Board.getInstance();
+		hand=player.getHand();
 
 	}
 
-	public void doStandardMovement(Scanner inputScanner) {	//do we need back capabilities
-		int userInput; //Initialized as invalid 
+	public void doStandardMovement() {	//do we need back capabilities
 		LinkedList<Tile> moveableTiles = player.getStandardMoveableTiles(); 	//Gets move able tiles and prints them out
 
-		System.out.println("/n enter" + moveableTiles.size() + "[Return] to Cancel Movement"); //Will always be one greater than last tile option
-		userInput=acceptableInput(0,moveableTiles.size()); //Ensures an acceptable input has been entered
+		System.out.println("\nEnter : [" + moveableTiles.size() + "] +  [Return] to Cancel Movement"); //Largest accpetable input is always to cance
+		userInput=Utility.acceptableInput(0,moveableTiles.size()); //Ensures an acceptable input has been entered
 
-		if(userInput==moveableTiles.size()) return; 	//cancels action
+		if(userInput==moveableTiles.size()) {
+			System.out.println("You cancelled");
+			return; //cancels action
+		} 	
 
-		System.out.println("Your pawn has been moved to " + moveableTiles.get(userInput).getName());
-		player.movePlayerPawn(moveableTiles.get(userInput)); 
+		selectedTile=moveableTiles.get(userInput);
+		System.out.println("Your pawn has been moved to " + selectedTile.getNameString());
+		player.movePlayerPawn(selectedTile); 
 		player.decrementPlayerActions();
 	}
 
-	public void doForcedMovement(Scanner inputScanner) {
-		int userInput;
+	public void doForcedMovement() {
 		LinkedList<Tile> moveableTiles = player.getFocredMoveableTile();
 
-		userInput=acceptableInput(0,moveableTiles.size()-1);	//Ensures valid Input -1 is to remove option to cancel movement as it is forced
+		userInput=Utility.acceptableInput(0,moveableTiles.size()-1);	//Ensures valid Input -1 is to remove option to cancel movement as it is forced
 
-		if(userInput==moveableTiles.size()) return; 	//cancels action
-
-		System.out.println("Your pawn has been moved to " + moveableTiles.get(userInput).getName());
-		player.movePlayerPawn(moveableTiles.get(userInput));
+		if(userInput==moveableTiles.size()) { 
+			return; //Cancels Action
+		}
+		selectedTile=moveableTiles.get(userInput);
+		System.out.println("Your pawn has been moved to " + selectedTile.getNameString());
+		player.movePlayerPawn(selectedTile);
 	}
 
-	public void shoreUpTiles(Scanner inputScanner) {
-		int userInput;
+	public void shoreUpTiles() {
 		LinkedList<Tile> shoreableTiles = player.getShoreableTiles();	//Gets shore able tiles
 
 		System.out.println("/n enter" + shoreableTiles.size() + "[Return] to cancel action");
-		userInput=acceptableInput(0, shoreableTiles.size());
-		System.out.println("You have shored up:" + shoreableTiles.get(userInput));
-		shoreableTiles.get(userInput).shoreUp(); //shores up the tile
+		userInput=Utility.acceptableInput(0, shoreableTiles.size());
 
-		if(userInput==shoreableTiles.size()) return; 	//cancels action
 
-		if(player instanceof Engineer) {	//Engineer may shore up two tiles
+		if(userInput==shoreableTiles.size()) { 
+			return; 	//cancels action
+		}
+		selectedTile=shoreableTiles.get(userInput);
+		System.out.println("You have shored up:" + selectedTile);
+		selectedTile.setFlood(false); //shores up the tile add shore up method
+
+		if(player instanceof Engineer) {	//Engineer may shore up two tiles //SMELL
 			System.out.println("/nYou are an Engineer so you may shore up another tile");
 			shoreableTiles = player.getShoreableTiles();	//must update
-			userInput=acceptableInput(0, shoreableTiles.size()-1);	//Cannot cancel since a tile has already been shore up
-			System.out.println("You have shored up:" + shoreableTiles.get(userInput));
+			userInput=Utility.acceptableInput(0, shoreableTiles.size()-1);	//Cannot cancel since a tile has already been shore up
+			selectedTile=shoreableTiles.get(userInput);
+			System.out.println("You have shored up:" + selectedTile);
+			selectedTile.setFlood(false);
 		}
 		player.decrementPlayerActions();
 	}
 
-	public void giveCard(Scanner inputScanner) {
-		int userInput;
-		int userSecondInput;
-		LinkedList<Player> possiblePlayers = player.giveTreasureCard();
-		Hand hand=player.getHand();
+	public void giveCard() {
+		Player selectedPlayer;
+		TreasureDeckCard selectedCard;
 
-		if(possiblePlayers==null) {
-			System.out.println("There is no player you can give cards to");
-			return;
-		}
-		System.out.println("/n enter" + possiblePlayers.size() + "[Return] to cancel action");
-		userInput=acceptableInput(0, possiblePlayers.size());
-		Player selectedPlayer = possiblePlayers.get(userInput);
-
-		if(userInput==possiblePlayers.size()) return; 	//cancels action
-
-		System.out.println("You must know select a card from your hand \n");
-		hand.printHand();
-		userSecondInput=acceptableInput(0, hand.getCards().size());
-		hand.giveCard(hand.get(userSecondInput), selectedPlayer.getHand());	//adds the selected card to the other players hand
+		//System.out.println("/n enter" + possiblePlayers.size() + "[Return] to cancel action");
+		selectedPlayer=selectPlayer();
+		//if(userInput==possiblePlayers.size()) return; 	//cancels action
+		selectedCard=selectCard();
+		hand.giveCard(selectedCard, selectedPlayer.getHand());	//adds the selected card to the other players hand
 		player.decrementPlayerActions();
 	}
-	
+
 	public void captureTreasure() {
 		if(player.captureTreasure()==true){
 			System.out.println("You have successfully captured:"+  player.getPlayerPawnTile().getTreasure().getString());
@@ -100,30 +102,28 @@ public class PlayerAction {
 		System.out.println("You were unable to capture the treasure");
 	}
 
-	public static int acceptableInput(int min,int max) { //Checks if input is acceptable by using the size of the actions lists //Maybe intrface
-		int userInput=-1;
-		Scanner inputScanner = new Scanner(System.in);
-		boolean acceptableInput = false;
+	public Player selectPlayer() {
+		LinkedList<Player> possiblePlayers = player.giveTreasureCard();
+		Player selectedPlayer;
 
-		while(acceptableInput == false) {
+		userInput=Utility.acceptableInput(0, possiblePlayers.size());
+		selectedPlayer = possiblePlayers.get(userInput);
 
-			if(inputScanner.hasNextInt() == true) {	//Checks if what is entered is an integer
-				userInput = inputScanner.nextInt();
+		return selectedPlayer;
+	} 
 
-				if((userInput>=min && userInput <=max)) {	//Checks if it is within the acceptable range
-					acceptableInput=true;	//exits method
-				}
-				else {
-					System.out.println("Please enter a number of the range" + min+ "-" + max); //Will be invoked if an int is entered but not int the right range
-					continue;
-				}
-			}
-			else {
-				System.out.println("Please enter a number of the range" + min + "-" + max); //Will be invoked if a character is entered
-			}
-		}
-		return userInput;
+	public TreasureDeckCard selectCard() {
+		TreasureDeckCard selectedCard;
+		ArrayList<TreasureDeckCard> cardsInHand = hand.getCards();
+
+		System.out.println("You must know select a card from your hand \n");
+		hand.printHand();
+		userInput=Utility.acceptableInput(0, cardsInHand.size());
+		selectedCard=cardsInHand.get(userInput);
+
+		return selectedCard;
 	}
 
 
+	}
 }
