6d2ce5fd9a1537f40bf44ee73c8f53482e8b7e86
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-11-29 12:33:20 +0000

    Added a way to capture treasure in the TreasureNames enum class

--- src/island/enums/TreasureNames.java
+++ src/island/enums/TreasureNames.java
@@ -1,19 +1,44 @@
 package island.enums;
 
 public enum TreasureNames {
-	TheEarthStone ("The Earth Stone"),
-	TheStatueOfTheWind ("The Statue of The Wind"),
-	TheCrystalOfFire ("The Crystal of Fire"),
-	TheOceansChalice ("The Oceans Chalice");
+	TheEarthStone ("The Earth Stone", false),
+	TheStatueOfTheWind ("The Statue of The Wind", false),
+	TheCrystalOfFire ("The Crystal of Fire", false),
+	TheOceansChalice ("The Oceans Chalice", false);
 	
 	private final String name;
+	private boolean captured;
 	
-	private TreasureNames(String name) {
+	/**
+	 * Constructor for TreasureNames enum
+	 * @param name Name of the treasure
+	 * @param captured True if treasure is captured, false otherwise
+	 */
+	private TreasureNames(String name, boolean captured) {
 		this.name = name;
+		this.captured = captured;
 	}
 	
+	/**
+	 * Get string of the Treasure name
+	 * @return String name, name of the treasure
+	 */
 	public String getString() {
 		return name;
 	}
 	
+	/**
+	 * Checks if treasure has been captured
+	 * @return True if treasure is captured, false otherwise
+	 */
+	public boolean isCaptured() {
+		return this.captured;
+	}
+	
+	/**
+	 * Capture the treasure
+	 */
+	public void captureTreasure() {
+		this.captured = true;
+	}
 }
