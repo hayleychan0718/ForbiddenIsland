8951c0a14e67dfb240b50692f089942d3b066eaa
liamfitzz <liam.fitzgerald@ucdconnect.ie>
2020-12-13 13:16:11 +0000

    Now model for MVC playerSetup

--- src/setup/PlayerSetup.java
+++ src/setup/PlayerSetup.java
@@ -1,93 +1,56 @@
 package setup;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import javax.tools.DiagnosticCollector;
+
 import gameLogic.PlayerAction;
+import island.board.Board;
 import players.*;
-import players.PlayerList;
+import utility.Utility;
 
 public class PlayerSetup {
 
 	private PlayerList playerList;
-	private List<Integer> RandomIntList;
+	private ArrayList<String> roleList;
+	private static PlayerSetup playerSetup;
 
-	public PlayerSetup() {
-		playerList = PlayerList.getInstance();
-		IntStream theIntStream = IntStream.rangeClosed(0, 5);
-		List<Integer> RandomIntList =  theIntStream.boxed().collect(Collectors.toList()); //Create a list 0-5 using the intStream used for random player role
-	}
-
-
-	public int setNumofPlayers(Scanner scannerIn) {
-		int userInput;
-		Integer numOfPlayers = 0;
-		System.out.println("Enter the number of players playing, this must be be of the range 2 to 4:");
-
-		userInput=PlayerAction.acceptableInput(2, 4);
-		numOfPlayers=userInput;
 	
-		return numOfPlayers;
+	public static PlayerSetup getInstance() {
+		if(playerSetup == null)
+			playerSetup = new PlayerSetup();
+		return playerSetup;
 	}
-
-	public void createPlayer(int playerNumber, Scanner scannerIn) {
-
-		System.out.println("\n You are Player number:"+ playerNumber + "\n");
-		System.out.println("Please Enter Your Name:");
-		String playerName = scannerIn.nextLine();
-
-		Collections.shuffle(RandomIntList);	//Shuffle to randomize selection
-
-		switch(RandomIntList.get(0)){
-		case 0:
-			Diver diver = Diver.getInstance(playerName, playerNumber);
-			playerList.addPlayer(diver);
-			break;
-
-		case 1:
-			Engineer engineer = Engineer.getInstance(playerName, playerNumber);
-			playerList.addPlayer(engineer);
-			break;
-
-		case 3:
-			Explorer explorer = Explorer.getInstance(playerName, playerNumber);
-			playerList.addPlayer(explorer);
-			break;
-
-		case 4:
-			Messenger messenger = Messenger.getInstance(playerName, playerNumber);
-			playerList.addPlayer(messenger);
-			break;
-
-		case 5:
-			Navigator navigator = Navigator.getInstance(playerName, playerNumber);
-			playerList.addPlayer(navigator);
-			break;
-
-		default:
-			break;
-		}
-
-		RandomIntList.remove(0); //Makes that another player cannot be assigned that role by removing it from the lsit
-
+	
+	private PlayerSetup() {
+		playerList = PlayerList.getInstance();
+		roleList = new ArrayList<String>();
+		Collections.addAll(roleList, "Diver","Engineer","Explorer","Messenger","Navigator");
 	}
-
-	public void createPlayers(Scanner scannerIn) {
-		int numOfPlayers=0;
-		boolean areAllPlayersSetup = false;
-
-		while(!areAllPlayersSetup) {
-				numOfPlayers= setNumofPlayers(scannerIn);
-				for(int i = 0; i< numOfPlayers; i++) {
-					createPlayer(i, scannerIn);
-				}
-				areAllPlayersSetup=true;	
-		}
+	
+	public ArrayList<String> getRoleList(){
+		return roleList;
+	}
+	
+	public void removeRole(int zero) {
+		roleList.remove(zero);
+	}
+	
+	public void shuffleRoles() {
+		Collections.shuffle(roleList);
+	}
+	
+	public void addPlayer(Player player) {
+		playerList.addPlayer(player);
 	}
 }
+ 
 
+	
 
 
