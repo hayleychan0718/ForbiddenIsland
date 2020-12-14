b19e92fe8da4415cacb950d463595b1aa4cfe771
liamfitzz <liam.fitzgerald@ucdconnect.ie>
2020-12-14 15:35:51 +0000

    Added return statement for giveTreasure card if no other players

--- src/gameLogic/PlayerView.java
+++ src/gameLogic/PlayerView.java
@@ -1,6 +1,7 @@
 package gameLogic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -24,6 +25,8 @@ public class PlayerView {
 		return playerView;
 	}
 
+	//No Constructor
+
 	public PlayerController getController() {
 		return controller;
 	}
@@ -34,7 +37,7 @@ public class PlayerView {
 	}
 
 	public void printOptions() {
-		System.out.println("You have the following available options");
+		System.out.println("\nYou have the following available options");
 		System.out.println("[1] Move Pawn");
 		System.out.println("[2] Shore Up Tile");
 		System.out.println("[3] Give Treasure Card");
@@ -43,17 +46,14 @@ public class PlayerView {
 		System.out.println("[0] End Turn");
 	}
 
-	public void printStartOfTurn(Player player) {
-		System.out.println("It is now" + player + "'s turn \n");
-	}
-
 	public void selectOption(Scanner inputScanner, Player player) {
 
 		int userInput = Utility.acceptableInput(0, 6, inputScanner);
 
 		switch(userInput) {
 		case 0:
-			EndTurn();
+			endTurn(player); //Concurrent modification
+			break;
 		case 1:
 			doStandardMovement(inputScanner, player);
 			break;
@@ -91,66 +91,66 @@ public class PlayerView {
 		}
 		controller.decementPlayerAction(player);
 	}
-	
+
 	public boolean canDoTileAction(ArrayList<Tile> tileOptions, String typeOfTileAction) {
 
 		if(tileOptions.isEmpty()) {
-			System.out.println("There is no Tiles to " + typeOfTileAction);
+			System.out.println("There are no Tiles to " + typeOfTileAction);
 			return false;
 		}
 		else {
 			System.out.println("You can " +  typeOfTileAction + "the following Tiles");
 			Utility.printOptions(tileOptions);
-			System.out.println("/n enter" + tileOptions.size() + "[Return] to cancel action");
+			System.out.println("Enter " + tileOptions.size() + " to cancel action");
 			return true;
 		}
 
 	}
 	
-	public void doStandardMovement(Scanner inputScanner, Player player){ 
-		ArrayList<Tile> moveableTiles = controller.getStandardMoveTiles(player);
-		
-		if(!canDoTileAction(moveableTiles, "move to")) return;
-		
-		int userInput = Utility.acceptableInput(0, moveableTiles.size(), inputScanner);
-		if(userInput==moveableTiles.size()) return;
-		
-		Tile selectedTile = moveableTiles.get(userInput);
-		controller.movePlayerPawn(player, selectedTile);
-		System.out.println("Your pawn has been moved to " + selectedTile);
-		controller.decementPlayerAction(player);
+	public void doStandardMovement(Scanner inputScanner, Player player){  
+		ArrayList<Tile> moveableTiles = controller.getStandardMoveTiles(player); //Sand Bag will get tiles you can shore up
+
+		if(!canDoTileAction(moveableTiles, "move to ")) return; //Make method similar to mine to check if it is empty, if empty return
+
+		int userInput = Utility.acceptableInput(0, moveableTiles.size(), inputScanner); // Get user input for the options
+		if(userInput==moveableTiles.size()) return;  //If they want to cancel Movement
+
+		Tile selectedTile = moveableTiles.get(userInput); //What the user selected, so the tile choose to shore up.
+		controller.movePlayerPawn(player, selectedTile);  //Controller then shore up selected tile
+		System.out.println("Your pawn has been moved to " + selectedTile); //The tile shored up
+		controller.decementPlayerAction(player); //Use controller to discard card.
 	}
-	
+
 	public void doForcedMovement(Scanner inputScanner, Player player) {
 		ArrayList<Tile> moveableTiles = controller.getForcedMovementTiles(player);
-		
-		if(!canDoTileAction(moveableTiles,"move to")) return;
-		
+		System.out.println("\n" + player + " must move their tile has been sunk!");
+
+		Utility.printOptions(moveableTiles);
 		int userInput = Utility.acceptableInput(0, moveableTiles.size()-1, inputScanner); //No option to cancel movement
-		
+
 		Tile selectedTile = moveableTiles.get(userInput);
 		controller.movePlayerPawn(player, selectedTile);
 		System.out.println("Your pawn has been moved to " + selectedTile);
 	}
-	
+
 	public void CaptureTreasure(Player player) {
-		
+
 		if(controller.canCaptureTreasure(player)) {
-			System.out.println("You have captured the treasure:" + controller.getTreasure(player));
+			System.out.println("You have captured the treasure:" + controller.getTileTreasure(player));
 			controller.decementPlayerAction(player);
 			return;
 		}
-		if(controller.getTreasure(player)==null) {
-			System.out.println("You are not on a treasure tile\n");
+		if(controller.getTileTreasure(player)==null) {
+			System.out.println("Your pawn is not on a treasure tile");   //WHat if treasur eis already captured
 			return;
 		}
-		System.out.println("You dont have the required card to capture the treasure");
+		System.out.println("You dont have the required card's to capture the treasure");
 		return;
 	}
-	
+
 	public Player selectPlayerForTreasureCard(Player player, Scanner inputScanner) {
 		ArrayList<Player> playersForTreasureCard =controller.getPlayerForTreasureCard(player);
-		
+
 		if (playersForTreasureCard.isEmpty()) {
 			System.out.println("There are no players to give treasure cards to");
 			return null;
@@ -160,22 +160,26 @@ public class PlayerView {
 		Player selectedPlayer = playersForTreasureCard.get(userInput);
 		return selectedPlayer;
 	}
-	
 
+	//Needs to be altered slightly should check i
 	public void giveTreasureCard(Player player , Scanner inputScanner) {
-		 ArrayList<TreasureDeckCard> currentPlayersCards = player.getHand().getCards();
 		
 		Player selectedPlayer = selectPlayerForTreasureCard(player,inputScanner);
+		if(selectedPlayer==null) return;
+		
+		ArrayList<TreasureDeckCard> currentPlayersCards = player.getHand().getCards();
+
+		
 		Hand selectedPlayerHand = controller.getPlayerHand(selectedPlayer);
 
 		if(!canDoCardAction(currentPlayersCards)) return;
-	
+
 		int userInput = Utility.acceptableInput(0, currentPlayersCards.size(), inputScanner);
 		TreasureDeckCard selectedCard = currentPlayersCards.get(userInput);
 		controller.giveCard(selectedCard, selectedPlayerHand, player);
 		controller.decementPlayerAction(player);
 	}
-	
+
 	public boolean canDoCardAction(ArrayList<TreasureDeckCard> cardOptions) {
 
 		if(cardOptions.isEmpty()) {
@@ -185,18 +189,50 @@ public class PlayerView {
 		else {
 			System.out.println("You can give the following cards:");
 			Utility.printOptions(cardOptions);
-			System.out.println("/n enter" + cardOptions.size() + "[Return] to cancel action");
+			System.out.println("Enter" + cardOptions.size() + " to cancel action");
 			return true;
 		}
 
 	}
+	
+	public void showBoard() {
+		Tile[][] board = controller.getboard();
+		int[][] islandTiles = controller.getIslandTiles();
 
-	public static void main(String[] args) {
-		Player Liam = new Player("Liam",1); 
-		ArrayList<Player> optionList = new ArrayList<Player>();
-		optionList.add(Liam);
-
-		PlayerView view = PlayerView.getInstanace();
-		Utility.printOptions(optionList);
+		for(int[] x: islandTiles) {
+			System.out.println(board[x[0]][x[1]].getName());
+			System.out.println("Coord: " + Arrays.toString(board[x[0]][x[1]].getLocation()));
+			System.out.println("Flooded: " + board[x[0]][x[1]].isFlooded());
+			System.out.println("Present: " + board[x[0]][x[1]].isPresent());
+			System.out.println();
+		}
 	}
-}
+		
+	public void endTurn(Player player) {
+		controller.emptyActions(player);
+	}
+
+//	public static void main(String[] args) {
+//		Player Liam = new Player("Liam",1); 
+//		ArrayList<Player> optionList = new ArrayList<Player>();
+//		optionList.add(Liam);
+//
+//		PlayerView view = PlayerView.getInstanace();
+//		Utility.printOptions(optionList);
+//	}
+
+	
+	
+
+
+
+		/*for(int i=0; i<6; i++) {
+	        	System.out.println();
+	        	for(int j=0; j<6; j++) {
+	        		if(board[i][j] == null) 
+	        			System.out.printf("%25s", "blank");
+	        		else 
+	        			System.out.printf("%25s", board[i][j].getName().getString());
+	        	}
+	        } */   	
+	}
