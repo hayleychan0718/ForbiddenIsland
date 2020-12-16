d449165ad9f76b7bac54abd6c91213bf167770c8
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-16 21:24:10 +0000

    GameOverObserver class

--- src/observers/GameObserver.java
+++
@@ -1,9 +0,0 @@
-package observers;
-
-
-public class GameObserver{
-	
-	public void update() {
-		loseGame(); // Invoke game lost method - from GameManager?
-	}
-}
---
+++ src/observers/GameOverObserver.java
@@ -0,0 +1,51 @@
+package observers;
+
+
+import gameLogic.PlayerView;
+import island.board.Board;
+import island.board.Tile;
+import island.enums.TileNames;
+import island.enums.TreasureNames;
+import players.Player;
+import players.PlayerList;
+
+import java.util.ArrayList;
+
+
+public class GameOverObserver {
+	public java.util.List<Player> playersList = PlayerList.getInstance().getListOfPlayers();
+	public Board board = Board.getInstance();
+	public ArrayList<Tile> sunkTiles = Board.getInstance().listOfSunkTiles();
+	
+	// TODO: Clean loops
+	public void update(TileNames tile) {		
+		for(Player player: playersList) {
+			for(Tile t: player.getShoreableTiles()) {
+				if(tile.getString() == t.getNameString()) {
+					if(PlayerView.getInstanace().notifyPlayer(tile, player) == false) {
+						if(loseCondition())
+							PlayerView.getInstanace().treasureLost();
+						if(tile.getString() == "Fool's Landing")
+							PlayerView.getInstanace().foolsLost();
+					}
+				}
+			}
+		}
+	}
+	
+	public void update() {
+		PlayerView.getInstanace().waterLost();
+	}
+	
+	public boolean loseCondition() {
+		if(sunkTiles.contains(board.getTile("Temple of The Sun")) && sunkTiles.contains(board.getTile("Temple of The Moon")) && !TreasureNames.TheEarthStone.isCaptured() ||
+				sunkTiles.contains(board.getTile("Cave of Shadows")) && sunkTiles.contains(board.getTile("Cave of Embers")) && !TreasureNames.TheCrystalOfFire.isCaptured() ||
+				sunkTiles.contains(board.getTile("Coral Palace")) && sunkTiles.contains(board.getTile("Tidal Palace")) && !TreasureNames.TheOceansChalice.isCaptured() ||
+				sunkTiles.contains(board.getTile("Howling Garden")) && sunkTiles.contains(board.getTile("Whispering Garden")) && !TreasureNames.TheStatueOfTheWind.isCaptured()){
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+}
