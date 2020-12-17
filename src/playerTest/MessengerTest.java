70a953bdf84e88914eca75430100df2932f83fc3
liamfitzz <liam.fitzgerald@ucdconnect.ie>
2020-12-17 20:32:12 +0000

    Messenger testing done

---
+++ src/playerTest/MessengerTest.java
@@ -0,0 +1,31 @@
+package playerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import players.Diver;
+import players.Engineer;
+import players.Messenger;
+import players.PlayerList;
+
+public class MessengerTest {
+
+	@Test
+	public void test() {
+		Messenger tester1 =  Messenger.getInstance("Test1", 1);
+		Diver tester2 =  Diver.getInstance("Test2", 2);
+		Engineer tester3 =  Engineer.getInstance("Test3", 2);
+		
+		PlayerList playerList = PlayerList.getInstance();
+		
+		playerList.addPlayer(tester1);
+		playerList.addPlayer(tester2);
+		playerList.addPlayer(tester3);
+		
+		Assert.assertTrue("The list of players for treasure card should be all the oother players", tester1.getPlayersForTreasureCard().equals(playerList.getListOfOtherPlayers(1)));
+		
+	}
+
+}
