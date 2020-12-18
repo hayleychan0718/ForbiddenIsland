84210a2f09dd34a98c7b24f5bd69dcff4e3104e1
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-18 10:33:10 +0000

    Adding GameOverObserver class

---
+++ src/observers/GameOverObserver.java
@@ -0,0 +1,53 @@
+package observers;
+
+
+import gameLogic.PlayerView;
+import island.board.Board;
+import island.board.Tile;
+import island.enums.TileNames;
+import island.enums.TreasureNames;
+import players.Player;
+import players.PlayerList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class GameOverObserver {
+	public java.util.List<Player> playersList = PlayerList.getInstance().getListOfPlayers();
+	
+	// TODO: Clean loops
+	public void update(Scanner intputScanner ,Tile tile) {
+		PlayerView playerView = PlayerView.getInstanace();
+		for(Player player: playersList) {
+			for(Tile t: player.getShoreableTiles()) {
+				if(tile == t) {
+					if(playerView.notifyPlayer(inputScanner, tile, player) == false) {
+						if(loseCondition())
+							PlayerView.getInstanace().treasureLost();
+						if(tile.getNameString() == "Fool's Landing")
+							PlayerView.getInstanace().foolsLost();
+					}
+				}
+			}
+		}
+	}
+	
+	public void update() {
+		PlayerView.getInstanace().waterLost();
+	}
+	
+	public boolean loseCondition() {
+		Board board = Board.getInstance();
+		ArrayList<Tile> sunkTiles = Board.getInstance().listOfSunkTiles();
+		if(sunkTiles.contains(board.getTile("Temple of The Sun")) && sunkTiles.contains(board.getTile("Temple of The Moon")) && !TreasureNames.TheEarthStone.isCaptured() ||
+				sunkTiles.contains(board.getTile("Cave of Shadows")) && sunkTiles.contains(board.getTile("Cave of Embers")) && !TreasureNames.TheCrystalOfFire.isCaptured() ||
+				sunkTiles.contains(board.getTile("Coral Palace")) && sunkTiles.contains(board.getTile("Tidal Palace")) && !TreasureNames.TheOceansChalice.isCaptured() ||
+				sunkTiles.contains(board.getTile("Howling Garden")) && sunkTiles.contains(board.getTile("Whispering Garden")) && !TreasureNames.TheStatueOfTheWind.isCaptured()){
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+}
