cc7019c1e96b8e4818314cb9d34315bc823ea556
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-18 10:32:26 +0000

    TreasureDeckSetup renamed to HandSetup

--- src/setup/HandSetup.java
+++ src/setup/HandSetup.java
@@ -2,32 +2,34 @@ package setup;
 
 import java.util.List;
 
+import island.cards.Card;
 import island.cards.TreasureDeck;
-import island.cards.TreasureDeckCard;
 import players.Player;
 import players.PlayerList;
 
-public class TreasureDeckSetup {
+public class HandSetup {
 	/*
 	 * Instance variables
 	 */
 	private TreasureDeck treasureDeck;
+	private List<Player> listOfPlayers;
+
 	
 	/**
-	 * Constructor for TreasureDeckSetup
+	 * Constructor for HandSetup
 	 */
-	public TreasureDeckSetup() {
+	public HandSetup() {
 		this.treasureDeck = TreasureDeck.getInstance();
+		this.listOfPlayers = PlayerList.getInstance().getListOfPlayers();
 	}
 	
 	/**
 	 * Give 2 cards to each player at the beginning of the game
 	 */
 	public void startGame() {
-		List<Player> listOfPlayers = PlayerList.getInstance().getListOfPlayers();
 		for(int i=0;i<listOfPlayers.size();i++) {
 			for(int j=0; j<2; j++) {
-				TreasureDeckCard card = treasureDeck.getStack().pop();
+				Card card = treasureDeck.getStack().pop();
 				listOfPlayers.get(i).getHand().addCard(card);
 			}
 		}
