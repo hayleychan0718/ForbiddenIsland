ba157a0c7ee726325b15ad309a82d6da749e92bc
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-20 03:08:32 +0000

    FloodSetup is singleton

--- src/setup/FloodSetup.java
+++ src/setup/FloodSetup.java
@@ -5,19 +5,26 @@ import island.board.Tile;
 import island.cards.FloodCard;
 import island.cards.FloodDeck;
 
-
 public class FloodSetup {
 	/*
 	 * Instance variables
 	 */
 	private FloodDeck floodDeck;
-	
+	private static FloodSetup floodSetup;
+
 	/**
 	 * Constructor for FloodSetup
 	 */
 	public FloodSetup() {
 		this.floodDeck = FloodDeck.getInstance();
 	}
+	
+    public static FloodSetup getInstance(){
+        if(floodSetup == null){
+        	floodSetup = new FloodSetup();
+        }
+        return floodSetup;
+    }
 	
 	/**
 	 * Flooding at start of game
