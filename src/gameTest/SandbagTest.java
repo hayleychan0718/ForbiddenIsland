8da8da7eeb936d1d77f5a67619c673f70042835a
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-21 23:12:16 +0000

    Sandbag card Unit Test

---
+++ src/gameTest/SandbagTest.java
@@ -0,0 +1,27 @@
+package gameTest;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import island.board.Board;
+import island.board.Tile;
+import island.cards.SandbagCard;
+
+public class SandbagTest {
+	// Tests that sandbag card shores up the tile
+	@SuppressWarnings("static-access")
+	@Test
+	public void testSandbagCard() {
+		Board board = Board.getInstance();
+		Tile tile = board.getTile("Whispering Garden");
+		tile.setFlood(true);
+		assertTrue("The tile is flooded", board.listOfFloodedTiles().contains(tile));
+
+		// Use the sandbag card to shore up the tile
+		SandbagCard sandbagCard = new SandbagCard();
+		sandbagCard.play(tile);
+		assertFalse("The tile is not flooded anymore", board.listOfFloodedTiles().contains(tile));
+	}
+}
