62abb1cfa7963f2efb2382d23f3dff8962bc437d
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-22 14:54:28 +0000

    TreasureDeckTest modified

--- src/cardTests/TreasureDeckTest.java
+++ src/cardTests/TreasureDeckTest.java
@@ -1,6 +1,7 @@
 package cardTests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -9,6 +10,7 @@ import org.junit.Test;
 import island.cards.Card;
 import island.cards.Hand;
 import island.cards.TreasureDeck;
+import island.cards.WaterRiseCard;
 import players.Engineer;
 
 public class TreasureDeckTest {
@@ -20,11 +22,14 @@ public class TreasureDeckTest {
 		
 		// Draw 2 cards from treasure deck
 		TreasureDeck treasureDeck = TreasureDeck.getInstance();
-		ArrayList<Card> cardDrawn = treasureDeck.drawCard(player);
+		ArrayList<Card> cardsDrawn = treasureDeck.drawCard(player);
 		// Add the cards to player's hand
 		Hand playerHand = player.getHand();
 		ArrayList<Card> playerCards = playerHand.getCards();
-		
-		assertEquals("The card drawn from treasure deck is in Player 1's hand", cardDrawn, playerCards);
+		// Makes sure water rise card isn't put into player's hand
+		for(Card card: cardsDrawn) {
+			if(!(card instanceof WaterRiseCard))
+				assertTrue("Treasure deck card drawn is in the player's hand", playerCards.contains(card));
+		}
 	}
 }
