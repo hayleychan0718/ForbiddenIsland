eeb6894ac69e064ae145db5f7682069aeecac960
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-22 12:23:27 +0000

    JavaDoc comments for Flood & Hand Setups and GameOverObserver

--- src/observers/GameOverObserver.java
+++ src/observers/GameOverObserver.java
@@ -1,24 +1,26 @@
 package observers;
 
 
-import players.PlayerView;
-import island.board.Board;
 import island.board.Tile;
-import island.enums.TreasureNames;
-import players.Player;
-import players.PlayerList;
-
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 import gameLogic.GameView;
 
-
+/**
+ * Observer to check if game is over
+ * @author Hayley Chan and Liam Fitzgerald
+ *
+ */
 public class GameOverObserver {
+	/*
+	 * Instance variables
+	 */
 	private static GameOverObserver gameOverObserver;
 	private ArrayList<Tile> sunkTiles = new ArrayList<Tile>();
 	
+	/**
+	 * Creating and instance of the game over observer
+	 * @return
+	 */
 	public static GameOverObserver getInstance() {
 		if(gameOverObserver==null) {
 			gameOverObserver = new GameOverObserver();
@@ -26,15 +28,27 @@ public class GameOverObserver {
 		return gameOverObserver;
 	}
 	
+	/**
+	 * When a tile is about to sink, the observer is updated
+	 * @param tile The tile that is about to sink
+	 */
 	public void update(Tile tile) {
 		sunkTiles.add(tile);
 	}
 	
+	/**
+	 * When water meter level has reached 5, observer is updated
+	 * @return True: water level has reached 5.
+	 */
 	public boolean update() {
 		GameView.getInstance().waterLost();
 		return true;
 	}
 	
+	/**
+	 * Gets a list of all the tiles that are sunk
+	 * @return List of sunken tiles
+	 */
 	public ArrayList<Tile> getSunkTiles(){
 		return sunkTiles;
 	}
