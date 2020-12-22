681f4f29f21ecc08476385699314d6ff5e50b418
liamfitzz <liam.fitzgerald@ucdconnect.ie>
2020-12-22 17:44:43 +0000

    playerObserver documentation

--- src/observers/PlayerObserver.java
+++ src/observers/PlayerObserver.java
@@ -1,38 +1,59 @@
 package observers;
 
 import java.util.ArrayList;
-
 import players.Player;
-import players.PlayerList;
+
+/**
+ * Singleton Observer class, used to observer the players tiles if sunk
+ * @author Liam Fitzgerald and Hayley Chan
+ *
+ */
 
 public class PlayerObserver {
 
-		private static PlayerObserver playerObserver;
-		private ArrayList<Player> sunkenPlayers;
-		
-	
-		private PlayerObserver() {
-			sunkenPlayers = new ArrayList<Player>();
+	private static PlayerObserver playerObserver;
+	private ArrayList<Player> sunkenPlayers;
+
+
+	/**
+	 * PlayerObserver Constructor
+	 */
+	private PlayerObserver() {
+		sunkenPlayers = new ArrayList<Player>();
+	}
+
+	/**
+	 * Gets instance of the PlayerObserver
+	 * @return playerObserver
+	 */
+	public static PlayerObserver getInstance() {
+		if(playerObserver==null) {
+			playerObserver = new PlayerObserver();
 		}
-		
-		public static PlayerObserver getInstance() {
-			if(playerObserver==null) {
-				playerObserver = new PlayerObserver();
-			}
-			return playerObserver;
-		}
-		
-		
-		public void updateSunk (Player player) {
-			sunkenPlayers.add(player);
-			
-		}
-		
-		public void updateMoved () {
-			sunkenPlayers.removeAll(sunkenPlayers);
-		}
-		
-		public ArrayList<Player> getSunkenPlayers (){
-			return sunkenPlayers;
-		}
+		return playerObserver;
+	}
+
+	/**
+	 * Updates if a player is sunk , adds them to sunkenPlayers List
+	 * @param player
+	 */
+	public void updateSunk (Player player) {
+		sunkenPlayers.add(player);
+
+	}
+
+	/**
+	 * Updates for when all of the players have mode , empties the list of sunken players
+	 */
+	public void updateMoved () {
+		sunkenPlayers.removeAll(sunkenPlayers);
+	}
+
+	/**
+	 * Gets the list of sunken Players
+	 * @return sunkenPlayers
+	 */
+	public ArrayList<Player> getSunkenPlayers (){
+		return sunkenPlayers;
+	}
 }
