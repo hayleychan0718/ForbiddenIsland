7595d23e9932893db2ad1caf26c59eb1437a0e14
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-22 03:00:49 +0000

    Extra Doc comments in HelicopterCard

--- src/island/cards/HelicopterCard.java
+++ src/island/cards/HelicopterCard.java
@@ -5,12 +5,24 @@ import java.util.ArrayList;
 import island.board.Tile;
 import players.Player;
 
+/**
+ * Helicopter Card subclass of TreasureDeckCard
+ * @author Hayley Chan and Liam Fitzgerald
+ *
+ */
 public class HelicopterCard extends TreasureDeckCard{
-	
+	/**
+	 * Helicopter card constructor
+	 */
 	public HelicopterCard() {
 		super.name = "Helicopter Lift";
 	}
 	
+	/**
+	 * Play a helicopter card. Move chosen player(s) to a chosen tile on the board.
+	 * @param chosenTile The tile to move to
+	 * @param chosenPlayers The players to move
+	 */
 	public static void play(Tile chosenTile, ArrayList<Player> chosenPlayers) {
 		for (Player player: chosenPlayers) {
 			player.movePlayerPawn(chosenTile);
