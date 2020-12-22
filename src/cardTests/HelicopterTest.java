ddbf34ba2ee46d8e9c8b9c31b6bd693756574dc8
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-22 01:34:00 +0000

    Helicopter Unit testing

---
+++ src/cardTests/HelicopterTest.java
@@ -0,0 +1,41 @@
+package cardTests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import island.board.Board;
+import island.board.Tile;
+import island.cards.HelicopterCard;
+import players.Engineer;
+import players.Navigator;
+import players.Player;
+import players.PlayerList;
+
+/**
+ * Unit test for playing helicopter card
+ * @author Hayley Chan and Liam Fitzgerald
+ *
+ */
+public class HelicopterTest {
+	@Test
+	public void movePlayersToTileTest() {
+		// Tile to move to 
+		Board board = Board.getInstance();
+		Tile tile = board.getTile("Watch Tower");
+		// Creating players
+		Engineer player1 = Engineer.getInstance("Player 1", 1, "$");
+		Navigator player2 = Navigator.getInstance("Player 2", 2, "%");
+		ArrayList<Player> chosenPlayers = new ArrayList<Player>();
+		chosenPlayers.add(player1);
+		chosenPlayers.add(player2);
+		
+		// Play helicopter card
+		HelicopterCard.play(tile, chosenPlayers);
+		
+		assertEquals("Player 1 is on chosen tile", tile, player1.getPlayerPawnTile());
+		assertEquals("Player 2 is on chosen tile", tile, player2.getPlayerPawnTile());
+	}
+}
