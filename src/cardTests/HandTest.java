9a70179d96ef832d70dd4df160dab093853a4827
hayleychan0718 <hayley.chan@ucdconnect.ie>
2020-12-22 01:16:53 +0000

    Edit Hand Unit test to cardTests package

--- src/cardTests/HandTest.java
+++ src/cardTests/HandTest.java
@@ -1,9 +1,7 @@
-package gameTest;
+package cardTests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
 
 import org.junit.Test;
 
